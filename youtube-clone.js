dc9ce1ac1916c4d5e2085e4d2fa79d76814c3d7a
rongkuan jiang <r75jiang@uwaterloo.ca>
2025-01-12 21:02:58 -0500

    decided that writing js for this clone is a waste of time since im already doing an amazon clone and it's very similar

---
+++ youtube-clone.js
@@ -0,0 +1,12 @@
+/*let fullHTML = '';
+
+
+function generateHTML(listOfVideos) {
+	document.querySelector('.js-')
+	listOfVideos.forEach((video) => {
+		const html = `
+		
+		`;
+		fullHTML += html;
+	});
+}*/
